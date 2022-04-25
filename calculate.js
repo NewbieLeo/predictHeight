ffbee88f6c9de16f910856e49bac228095759ed4
newbieleo <jogangmin07@gmail.com>
2022-04-26 02:02:55 +0900

    dev.0.2

--- calculate.js
+++ calculate.js
@@ -1,8 +1,15 @@
-var array1 = ["male1<br/>", "male2<br/>", "male3<br/>"];
-var array2 = ["female1<br/>", "female2<br/>", "female3<br/>"];
+var array1 = [
+    "male1<br/>", "male2<br/>", "male3<br/>",
+    "male4<br/>", "male5<br/>", "male6<br/>",
+    "male7<br/>", "male8<br/>", "male9<br/>"
+];
+var array2 = [
+    "female1<br/>", "female2<br/>", "female3<br/>",
+    "female4<br/>", "female5<br/>", "female6<br/>",
+    "female7<br/>", "female8<br/>", "female9<br/>"];
 
 $(document).ready(function () {
-    $("#btn").click(function() { // 
+    $("#btn").click(function() {
         var box = $(".box");
         var gen = $("input[name='gen']:checked").val();
         var isPuberty = $("input[name='isPuberty']:checked").val();
@@ -14,12 +21,12 @@ $(document).ready(function () {
         }
 
         if (gen == "male") {
-            for (let i=0; i<3; i++) {
+            for (let i=0; i<9; i++) {
                 //document.getElementById(`c${i}`).innerHTML = array1[i];
                 $(`#c${i}`).html(array1[i]);
             }
         } else if (gen == "female") {
-            for (let i=0; i<3; i++) {
+            for (let i=0; i<9; i++) {
                 // document.getElementById(`c${i}`).innerHTML = array2[i];
                 $(`#c${i}`).html(array2[i])
             }
@@ -27,6 +34,8 @@ $(document).ready(function () {
             alert("Please check your gender!")
             box[0].style.display = "none";
         }
+
+        
     });
 });
 function predictHeight() {
@@ -42,4 +51,9 @@ function predictHeight() {
         p_height = (f_height + m_height) / 2 - 6.5;
     }
     $("#result").html(p_height);
+    var mark = 0;
+    $("input[name='symptoms']:checked").each(function(i, iVal) {
+        mark += iVal.value;
+    });
+    console.log(mark);
 }
